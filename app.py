39cf8e503d9dd8baf51708e98dc5e7c2c238d5fa
Rohit <rohit_thapa007@hotmail.com>
2020-12-07 18:43:28 +0545

    print George

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-print("/___|")
-print("   /|")
-print("  / |")
-print(" /  |")
-
+print("There was a man named George")
+print("He was 70")
+print("He liked his name George.")
+print("but he didn't like being 70")
