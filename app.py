906b078a92b74d03f9d6796981e4dd707cf91545
Rohit <rohit_thapa007@hotmail.com>
2020-12-07 19:01:46 +0545

    String Functions

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-character_name = "John"
-character_age = "35"
-
-print("There was a man named "+character_name+",")
-print("He was "+character_age)
-print("He liked his name "+character_name+".")
-print("but he didn't like being "+character_age)
+phrase = "Girrafe Academy"
+print(phrase)
+print(phrase + " is cool.")
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.upper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("d"))
+print(phrase.index("a"))
+# print(phrase.index("z")) #error substring not found
+print(phrase.replace("Girrafe", "Elephant"))
