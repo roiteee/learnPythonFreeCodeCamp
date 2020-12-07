2098bdaa318ff43b79813f7d97d61ea8537231de
Rohit <rohit_thapa007@hotmail.com>
2020-12-07 20:14:36 +0545

    Basic Input

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
-from math import *
-
-print(max(4,6))
-print(min(5,10))
-print(round(3.2)) #3
-print(round(3.85)) #4
-
-#functions in math library
-print(floor(3.7)) #3 chops off decimal
-print(ceil(3.7)) #4
-print(sqrt(36)) #6
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello "+name+"! You're "+age+".") 
 
