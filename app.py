2b2760cdcfc7b70e0aea26bddcc0575c81c93cb0
Rohit <rohit_thapa007@hotmail.com>
2020-12-07 20:25:26 +0545

    Convert string to float and back

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-print("Hello "+name+"! You're "+age+".") 
+num1 = input("Enter a number: ") #by default this is string
+num2 = input("Enter another number: ")
+
+result = float(num1)+float(num2) #int if no decimal requirement
+print("The sum is " + str(result))
+
+
+
 
