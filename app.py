44e59d6abc89faf95cc6618aea0b0d57455ed80d
Rohit <rohit_thapa007@hotmail.com>
2020-12-13 00:58:51 +0545

    if else statements

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
-def cube(num):
-    return num*num*num
+is_male = False
+is_tall = False
+
+if is_male:
+    print("You are a male")
+else:
+    print("You are not a male")
+
+print("\n")
+if is_male or is_tall: #TF FT TT
+    print("You are a male or tall or both")
+else: #FF
+    print("You are neither male nor tall")
+
+print("\n")
+if is_male and is_tall: #TT
+    print("You are a tall male")
+elif is_male and not(is_tall): #TF
+    print("You're a short male")
+elif not(is_male) and is_tall: #FT
+    print("You're not a male but are tall")
+else: #FF
+    print("You're not a male and not are tall")
 
 
-cube(4)  #this statement is not assigned to anything or printed
-print(cube(43))    
