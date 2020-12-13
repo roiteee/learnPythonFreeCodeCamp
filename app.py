4aff8ade62ee0eba34ad3be94575ce2b1dea096a
Rohit <rohit_thapa007@hotmail.com>
2020-12-13 17:54:00 +0545

    Exponent function self made

--- app.py
+++ app.py
@@ -1,27 +1,13 @@
-for letter in "Giraffe":
-    print(letter)
+print(2**3)
 
+#Similar self made function
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result *= base_num
+    return result
 
-friends = ["Jim", "Karen", "Kevin"]
-for friend in friends:
-    print(friend)
-
-for index in range(4): # 0 1 2 3
-    print(index)
-
-
-for index in range(9, 15): #9 10 11 12 13 14
-    print(index)
-
-for index in range(len(friends)):
-    print(friends[index])
-
-for index in range(5):
-    if index==0:
-        print("First iteration")
-    else:
-        print("Not first")
-
+print(raise_to_power(2,3))
 
 
     
