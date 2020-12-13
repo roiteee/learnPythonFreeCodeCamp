dcce456700d76b7db0285157e441b81e1f85456d
Rohit <rohit_thapa007@hotmail.com>
2020-12-13 13:47:43 +0545

    Improved calculator + - * /

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-def max_num(num1, num2, num3):
-    if num1>=num2 and num1>=num3:
-        return num1
-    elif num2>=num1 and num2>=num3:
-        return num2
-    else:
-        return num3
+num1 = float(input("Enter 1st number: "))
+num2 = float(input("Enter 2nd number: "))
+op = input("Enter operator")
 
-print(max_num(561,10,3))
+if op == "+":
+    print(num1+num2)
+elif op == "-":
+    print(num1-num2)
+elif op == "/":
+    print(num1/num2)
+elif op == "*":
+    print(num1*num2)
+else:
+    print("Invalid operator")
