214ececbaa5d91c6cc60f6e2725a9f4a8870892c
Rohit <rohit_thapa007@hotmail.com>
2020-12-13 13:37:51 +0545

    Print max

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-is_male = False
-is_tall = False
+def max_num(num1, num2, num3):
+    if num1>=num2 and num1>=num3:
+        return num1
+    elif num2>=num1 and num2>=num3:
+        return num2
+    else:
+        return num3
 
-if is_male:
-    print("You are a male")
-else:
-    print("You are not a male")
-
-print("\n")
-if is_male or is_tall: #TF FT TT
-    print("You are a male or tall or both")
-else: #FF
-    print("You are neither male nor tall")
-
-print("\n")
-if is_male and is_tall: #TT
-    print("You are a tall male")
-elif is_male and not(is_tall): #TF
-    print("You're a short male")
-elif not(is_male) and is_tall: #FT
-    print("You're not a male but are tall")
-else: #FF
-    print("You're not a male and not are tall")
-
-
+print(max_num(561,10,3))
