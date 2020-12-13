6ed1b090e8bbd587a471a9c6db7ce689bcacd542
Rohit <rohit_thapa007@hotmail.com>
2020-12-13 14:33:19 +0545

    Guessing game with guess_limit

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 secret_word = "giraffe"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-while guess != secret_word:
-    guess = input("Enter Guess: ")
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit and not(out_of_guesses):
+        guess = input("Enter Guess: ")
+        guess_count += 1
+    else:
+        out_of_guesses = True
 
-print("You won") #Program comes here only when you've guessed right
+if out_of_guesses:
+    print("You lose")
+else:
+    print("You won")
