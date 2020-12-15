98fa29c60bed2cdfe6ad994b98f7c6095120a4c7
Rohit <rohit_thapa007@hotmail.com>
2020-12-16 01:55:29 +0545

    Multiple choice quiz

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-class Student:
-    def __init__(self, name, major, gpa, is_on_probation):    #Constructor
-        self.name = name
-        self.major = major
-        self.gpa = gpa
-        self.is_on_probation = is_on_probation
+from Question import Question
 
-student1 = Student("Jim", "Business", 3.1, False)
-print(student1.name)
+question_prompts = [
+    "What color are apples?\n(a)Red/Green (b)Purple (c)Orange\n",
+    "What color are bananas?\n(a)Teal(b)Magenta(c)Yellow\n",
+    "What color are strawberries?\n(a)Yellow(b)Red(c)Blue\n"
+    ]
 
-student2 = Student("Pam", "Art", 2.5, True)
-print(student2.gpa)
+questions = [
+    Question(question_prompts[0], "a"),
+    Question(question_prompts[1], "c"),
+    Question(question_prompts[2], "b")
+    ]
+
+
+def run_test(questions):
+    score = 0
+    for q in questions:
+        result=input(q.prompt)
+        if(result == q.answer):
+            score +=1
+    print("You got " + str(score) + "/" + str(len(questions)) + "correct.")
+
+        
+
+run_test(questions)
