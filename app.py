d068aaf623b77cfd1074d6c17e83fb5e008c0e86
Rohit <rohit_thapa007@hotmail.com>
2020-12-15 21:06:05 +0545

    writing html file

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ employee_file.close()
 employee_file = open('employees.txt', 'w') #overrites the file
 employee_file.write("Rick - Salesman")
 employee_file.close()
+
+employee_file = open('index.html', 'w') # writes html code
+employee_file.write("<p>This is HTML<p>")
+employee_file.close()
