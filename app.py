08185d6a21681dbb0e7b72c42ed62f2e160dab6a
Rohit <rohit_thapa007@hotmail.com>
2020-12-15 21:28:31 +0545

    using import

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-employee_file = open('employees.txt','a') #appends at the end of file
+import useful_tools # useful_tools.py file  
 
-employee_file.write("Toby - Human Resources")
-employee_file.write("\nKelly - Customer Services")  #\n puts next line
-employee_file.close()
-
-employee_file = open('employees.txt', 'w') #overrites the file
-employee_file.write("Rick - Salesman")
-employee_file.close()
-
-employee_file = open('index.html', 'w') # writes html code
-employee_file.write("<p>This is HTML<p>")
-employee_file.close()
+print(useful_tools.roll_dice(10))
