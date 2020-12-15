8c2c5731f1c7320a0c48ec3f6a9014fe76fa6169
Rohit <rohit_thapa007@hotmail.com>
2020-12-15 23:44:07 +0545

    Student class

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-#install module
-#pip install python-docx
-#uninstall
-#pip uninstall python-docx
+class Student:
+    def __init__(self, name, major, gpa, is_on_probation):    #Constructor
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
 
+student1 = Student("Jim", "Business", 3.1, False)
+print(student1.name)
+
+student2 = Student("Pam", "Art", 2.5, True)
+print(student2.gpa)
